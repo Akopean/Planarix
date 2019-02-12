d06a4fecc9d773d5a5fb300aaf1281d6fef2ff63
Akopean <agasi_dark@ukr.net>
2019-02-12 21:21:51 +0200

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+
+window.$ = window.jQuery = require('jquery');
+
+
+window.Popper = require('popper.js').default;
+
+require('bootstrap');
+
+
---
+++ webpack.mix.js
@@ -0,0 +1,21 @@
+
+let mix = require('laravel-mix');
+
+
+mix.webpackConfig({
+  node: {
+    fs: 'empty' //https://github.com/webpack-contrib/css-loader/issues/447 issue
+  }
+});
+
+mix.setPublicPath('./');
+
+mix.options({
+  processCssUrls: false,
+  fs : 'empty'
+});
+
+  
+mix.js('src/js/app.js', './js')
+   .sass('src/scss/app.scss', './css')
+    .copy('src/images', './images');
