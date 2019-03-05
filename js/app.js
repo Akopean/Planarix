5ba65d76599c5e61eaed4c45be0ae568d5f38bcb
Akopean <agasi_dark@ukr.net>
2019-03-05 18:57:52 +0200

    update

--- js/app.js
+++ js/app.js
@@ -18892,18 +18892,10 @@ $(".l-navbar--toggler").on("click", function () {
   $(".collapse.navbar-collapse").toggleClass('show');
 });
 $(".l_user-menu--toggle").on("click", function () {
-  $(".sidenav").toggleClass('show');
+  $("body").toggleClass('r-menu_show');
 });
 $(".l_other_account-login").tooltip({
   placement: "top"
-});
-$(function () {
-  // Bind the swipeleftHandler callback function to the swipe event on div.box
-  $("body").on("swipeleft", swipeleftHandler); // Callback function references the event target and adds the 'swipeleft' class to it
-
-  function swipeleftHandler(event) {
-    $(event.target).addClass("swipeleft");
-  }
 });
 
 /***/ }),
--- src/js/app.js
+++ src/js/app.js
@@ -22,20 +22,9 @@ $( ".l-navbar--toggler" ).on( "click", function() {
 });
 
 $( ".l_user-menu--toggle" ).on( "click", function() {
-    $( ".sidenav").toggleClass('show');
+    $( "body").toggleClass('r-menu_show');
 });
 
 $(".l_other_account-login").tooltip({
     placement: "top"
-});
-
-
-$(function(){
-    // Bind the swipeleftHandler callback function to the swipe event on div.box
-    $( "body" ).on( "swipeleft", swipeleftHandler );
-
-    // Callback function references the event target and adds the 'swipeleft' class to it
-    function swipeleftHandler( event ){
-        $( event.target ).addClass( "swipeleft" );
-    }
 });
