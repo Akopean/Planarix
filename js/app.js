5fc51ebd5abe107446f22e85322c97c3a3fb2e3d
Akopean <agasi_dark@ukr.net>
2019-03-26 13:56:31 +0200

    update

--- js/app.js
+++ js/app.js
@@ -18986,93 +18986,87 @@ eval(function (p, a, c, k, _e, r) {
 //Touch Right  Menu
 $(function () {
   var body_touch = {
-    touch_right_started: false,
-    touch_right_detecting: false,
-    touch_right_delta: null,
-    touch_right_delta_x: 0
+    touch_started: false,
+    touch_detecting: false,
+    touch_delta: null,
+    touch_delta_x: 0
   };
   var sidenav = {
-    touch_right_started: false,
-    touch_right_detecting: false,
-    touch_right_delta: null,
-    touch_right_delta_x: 0
+    touch_started: false,
+    touch_detecting: false,
+    touch_delta: null,
+    touch_delta_x: 0
   };
   /** body Touch  Right */
 
-  /* document.getElementById('l_site-main').addEventListener('touchstart', function (e) {
-       if (e.touches.length !== 1 || body_touch.touch_right_started) {
-           return;
-       }
-       body_touch.touch_right_detecting = body_touch.touch_right_started = true;
-       // Запоминаем текущее касание и его координаты
-       body_touch.touch_right_delta_x = e.changedTouches[0].pageX;
-   }, true);
-     document.getElementById('l_site-main').addEventListener('touchmove', function (e) {
-       if (!body_touch.touch_right_started && !body_touch.touch_right_detecting) {
-           return;
-       }
-         if (body_touch.touch_right_started) {
-           e.preventDefault();
-           let delta = body_touch.touch_right_delta_x - e.changedTouches[0].pageX;
-             if (delta > 0 || delta < 0) {
-               body_touch.touch_right_delta = delta / 5;
-           }
-       }
-   }, false);
-     document.getElementById('l_site-main').addEventListener('touchend', function (e) {
-       if (e.touches.length >= 1 && !body_touch.touch_right_started) {
-           return;
-       }
-       e.preventDefault();
-         body_touch.touch_right_delta < 0 && swiperightHandler();
-   }, false);
-  */
+  document.getElementById('l_site-main').addEventListener('touchstart', function (e) {
+    if (e.touches.length !== 1 || body_touch.touch_started) {
+      return;
+    }
 
+    body_touch.touch_detecting = true; // Запоминаем текущее касание и его координаты
+
+    body_touch.touch_delta_x = e.changedTouches[0].pageX;
+  }, false);
+  document.getElementById('l_site-main').addEventListener('touchmove', function (e) {
+    if (e.touches.length !== 1 && !body_touch.touch_detecting) {
+      return;
+    }
+
+    if (!body_touch.touch_started) {
+      e.preventDefault();
+      body_touch.touch_started = true;
+      body_touch.touch_delta = body_touch.touch_delta_x - e.changedTouches[0].pageX;
+    }
+  }, false);
+  document.getElementById('l_site-main').addEventListener('touchend', function (e) {
+    if (e.touches.length !== 1 && !body_touch.touch_started) {
+      return;
+    }
+
+    e.preventDefault();
+    body_touch.touch_started = body_touch.touch_detecting = false;
+    body_touch.touch_delta < 0 && swiperightHandler();
+  }, false);
   /** Sidenav  Touch  Left */
 
-  /*   document.getElementById('sidenav').addEventListener('touchstart', function (e) {
-         if (e.touches.length !== 1 || sidenav.touch_right_started) {
-             return;
-         }
-  
-         sidenav.touch_right_detecting = sidenav.touch_right_started = true;
-         // Запоминаем текущее касание и его координаты
-         sidenav.touch_right_delta_x = e.changedTouches[0].pageX;
-     }, false);
-  
-     document.getElementById('sidenav').addEventListener('touchmove', function (e) {
-         if (!sidenav.touch_right_started && !sidenav.touch_right_detecting) {
-             return;
-         }
-  
-         if (sidenav.touch_right_started) {
-             e.preventDefault();
-  
-             let delta = sidenav.touch_right_delta_x - e.changedTouches[0].pageX;
-  
-             if (delta > 0 || delta < 0) {
-                 sidenav.touch_right_delta = delta / 5;
-             }
-         }
-     }, false);
-  
-     document.getElementById('sidenav').addEventListener('touchend', function (e) {
-         if (e.touches.length >= 1 && !sidenav.touch_right_started) {
-             return;
-         }
-         e.preventDefault();
-  
-         sidenav.touch_right_delta > 0 && swipeleftHandler();
-     }, false);
-  
-     function swiperightHandler() {
-         $("body").addClass("r-menu_show");
-     }
-  
-     function swipeleftHandler() {
-         $("body").removeClass("r-menu_show");
-     }
-  */
+  document.getElementById('sidenav').addEventListener('touchstart', function (e) {
+    if (e.touches.length !== 1 || sidenav.touch_started) {
+      return;
+    }
+
+    sidenav.touch_detecting = true; // Запоминаем текущее касание и его координаты
+
+    sidenav.touch_delta_x = e.changedTouches[0].pageX;
+  }, false);
+  document.getElementById('sidenav').addEventListener('touchmove', function (e) {
+    if (e.touches.length !== 1 && !sidenav.touch_detecting) {
+      return;
+    }
+
+    if (!sidenav.touch_started) {
+      e.preventDefault();
+      sidenav.touch_started = true;
+      sidenav.touch_delta = sidenav.touch_delta_x - e.changedTouches[0].pageX;
+    }
+  }, false);
+  document.getElementById('sidenav').addEventListener('touchend', function (e) {
+    if (e.touches.length !== 1 && !sidenav.touch_started) {
+      return;
+    }
+
+    e.preventDefault();
+    sidenav.touch_started = sidenav.touch_detecting = false;
+    sidenav.touch_delta > 0 && swipeleftHandler();
+  }, false);
+
+  function swiperightHandler() {
+    $("body").addClass("r-menu_show");
+  }
+
+  function swipeleftHandler() {
+    $("body").removeClass("r-menu_show");
+  }
 });
 
 /***/ }),
--- src/js/touch.js
+++ src/js/touch.js
@@ -1,86 +1,83 @@
 //Touch Right  Menu
 $(function () {
     const body_touch = {
-        touch_right_started: false,
-        touch_right_detecting: false,
-        touch_right_delta: null,
-        touch_right_delta_x: 0
+        touch_started: false,
+        touch_detecting: false,
+        touch_delta: null,
+        touch_delta_x: 0
     };
     const sidenav = {
-        touch_right_started: false,
-        touch_right_detecting: false,
-        touch_right_delta: null,
-        touch_right_delta_x: 0
+        touch_started: false,
+        touch_detecting: false,
+        touch_delta: null,
+        touch_delta_x: 0
     };
     /** body Touch  Right */
-   /* document.getElementById('l_site-main').addEventListener('touchstart', function (e) {
-        if (e.touches.length !== 1 || body_touch.touch_right_started) {
-            return;
-        }
-        body_touch.touch_right_detecting = body_touch.touch_right_started = true;
-        // Запоминаем текущее касание и его координаты
-        body_touch.touch_right_delta_x = e.changedTouches[0].pageX;
-    }, true);
+     document.getElementById('l_site-main').addEventListener('touchstart', function (e) {
+         if (e.touches.length !== 1 || body_touch.touch_started) {
+             return;
+         }
+         body_touch.touch_detecting = true;
+         // Запоминаем текущее касание и его координаты
+         body_touch.touch_delta_x = e.changedTouches[0].pageX;
+     }, false);
 
-    document.getElementById('l_site-main').addEventListener('touchmove', function (e) {
-        if (!body_touch.touch_right_started && !body_touch.touch_right_detecting) {
-            return;
-        }
+     document.getElementById('l_site-main').addEventListener('touchmove', function (e) {
+         if (e.touches.length !== 1&& !body_touch.touch_detecting) {
+             return;
+         }
 
-        if (body_touch.touch_right_started) {
-            e.preventDefault();
-            let delta = body_touch.touch_right_delta_x - e.changedTouches[0].pageX;
+         if (!body_touch.touch_started) {
+             e.preventDefault();
+             body_touch.touch_started = true;
+             body_touch.touch_delta = body_touch.touch_delta_x - e.changedTouches[0].pageX;
 
-            if (delta > 0 || delta < 0) {
-                body_touch.touch_right_delta = delta / 5;
-            }
-        }
-    }, false);
+         }
+     }, false);
 
-    document.getElementById('l_site-main').addEventListener('touchend', function (e) {
-        if (e.touches.length >= 1 && !body_touch.touch_right_started) {
-            return;
-        }
-        e.preventDefault();
+     document.getElementById('l_site-main').addEventListener('touchend', function (e) {
+         if (e.touches.length !== 1 && !body_touch.touch_started) {
+             return;
+         }
+         e.preventDefault();
+         body_touch.touch_started = body_touch.touch_detecting = false;
+         body_touch.touch_delta < 0 && swiperightHandler();
+     }, false);
 
-        body_touch.touch_right_delta < 0 && swiperightHandler();
-    }, false);
-*/
 
     /** Sidenav  Touch  Left */
- /*   document.getElementById('sidenav').addEventListener('touchstart', function (e) {
-        if (e.touches.length !== 1 || sidenav.touch_right_started) {
-            return;
-        }
+      document.getElementById('sidenav').addEventListener('touchstart', function (e) {
 
-        sidenav.touch_right_detecting = sidenav.touch_right_started = true;
-        // Запоминаем текущее касание и его координаты
-        sidenav.touch_right_delta_x = e.changedTouches[0].pageX;
-    }, false);
+          if (e.touches.length !== 1 || sidenav.touch_started) {
+              return;
+          }
+
+          sidenav.touch_detecting = true;
+          // Запоминаем текущее касание и его координаты
+          sidenav.touch_delta_x = e.changedTouches[0].pageX;
+
+      }, false);
 
     document.getElementById('sidenav').addEventListener('touchmove', function (e) {
-        if (!sidenav.touch_right_started && !sidenav.touch_right_detecting) {
+        if (e.touches.length !== 1 && !sidenav.touch_detecting) {
             return;
         }
 
-        if (sidenav.touch_right_started) {
+        if (!sidenav.touch_started) {
             e.preventDefault();
-
-            let delta = sidenav.touch_right_delta_x - e.changedTouches[0].pageX;
-
-            if (delta > 0 || delta < 0) {
-                sidenav.touch_right_delta = delta / 5;
-            }
+            sidenav.touch_started = true;
+            sidenav.touch_delta = sidenav.touch_delta_x - e.changedTouches[0].pageX;
         }
     }, false);
 
     document.getElementById('sidenav').addEventListener('touchend', function (e) {
-        if (e.touches.length >= 1 && !sidenav.touch_right_started) {
+        if (e.touches.length !== 1 && !sidenav.touch_started) {
             return;
         }
-        e.preventDefault();
 
-        sidenav.touch_right_delta > 0 && swipeleftHandler();
+        e.preventDefault();
+        sidenav.touch_started = sidenav.touch_detecting = false;
+        sidenav.touch_delta > 0 && swipeleftHandler();
     }, false);
 
     function swiperightHandler() {
@@ -90,5 +87,5 @@ $(function () {
     function swipeleftHandler() {
         $("body").removeClass("r-menu_show");
     }
-*/
+
 });
